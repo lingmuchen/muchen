118304ae2200cefd40bdcb80bfe171a671023b79
lingmuchen <74294741+lingmuchen@users.noreply.github.com>
2020-11-19 19:50:30 +0800

    Create 111910.性别.c

---
+++ 111910.性别.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int m;
+	scanf("%d",&m);
+	printf("输入\n")
+	if(m==1)
+	{
+		printf("男\n");
+	}
+	else if(m==0)
+	{
+		printf("女\n");
+	}
+	else {
+		printf("无法判断\n");
+	}
+return 0;
+}
