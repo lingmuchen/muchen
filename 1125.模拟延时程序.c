a121cb5198628515d255b8d7928dba5749e53411
lingmuchen <74294741+lingmuchen@users.noreply.github.com>
2020-11-25 17:01:00 +0800

    Create 1125.模拟延时程序.c

---
+++ 1125.模拟延时程序.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<windows.h>
+int main(){
+printf("	         双生契\n");
+Sleep(3000);
+printf("	镜前燃着烛花 盔甲藏着晚霞\n");
+Sleep(2000);
+printf("	 窗外平凡人家 又几番冬夏\n");
+Sleep(2000);
+printf("	你驻马便是家 又何惧天大地大\n");
+Sleep(2000);
+printf("	咫尺天涯 将多少枯骨埋入风沙\n");
+return 0;
+}
