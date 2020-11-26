f80cd9f99c73a3e2ac16bf88cfdda8671d01d493
lingmuchen <74294741+lingmuchen@users.noreply.github.com>
2020-11-26 20:54:31 +0800

    Create 112604.降序排序.c

---
+++ 112604.降序排序.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(viod){
+	int m,l,c;
+	int z;
+	scanf("%d%d%d",&m,&l,&c);
+	if(m<l)
+	{z=m,m=l,l=z;}
+	if(m<c)
+	{z=m,m=c,c=z;}
+	if(l<c)
+	{z=l,l=c,c=z;}
+	printf("%d\n%d\n%d\n",m,l,c);
+return 0;
+}
