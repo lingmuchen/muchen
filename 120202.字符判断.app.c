e2ff2ece66333617c38b6c81a3dde0fcc3701c2e
lingmuchen <74294741+lingmuchen@users.noreply.github.com>
2020-12-02 09:20:28 +0800

    Create 120202.字符判断.app.c

---
+++ 120202.字符判断.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	char m;	
+	printf("	欢迎字符判断语句程序\n");
+	printf("请输入字符");
+	printf("	");
+	scanf("%c",&m);
+	switch(m){
+	  case'w':printf("	你在点击“上”键\n");break;
+	  case'a':printf("	你在点击“左”键\n");break;
+	  case'd':printf("	你在点击“右”键\n");break;
+	  case's':printf("	你在点击“下”键\n");break;
+	  default:printf("输入有误，请输入a w s d四个字符其中之一");
+	}
+	return 0;
+}
