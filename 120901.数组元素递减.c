4485ad64bc12dc1f3c9fe7258284d7eb25a079c1
lingmuchen <74294741+lingmuchen@users.noreply.github.com>
2020-12-09 09:09:54 +0800

    Create 120901.数组元素递减.c

---
+++ 120901.数组元素递减.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+int l,c;
+int m=90;
+int ling[9][10]={
+	{0},
+	{0}
+	};
+for(l=0;l<9;l=l++){
+	for(c=0;c<10;c=c++){
+		ling[l][c]=0;
+		printf("%d ",ling[l][c]);
+		}
+	{printf("\n");}
+	}
+printf("\n");
+	for(l=0;l<9;l=l++){
+	  for(c=0;c<10;c=c++){
+	    ling[l][c]=m--;
+		    printf("%d ",ling[l][c]);
+	}
+	printf("\n");
+}
+return 0;
+}
